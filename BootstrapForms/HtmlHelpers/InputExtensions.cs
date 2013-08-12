bfeb170b0748cebe16aaa9f5d225b7b357a9c1f3
Stefan Prodan <stefan.prodan@gmail.com>
2013-08-12 17:21:46 +0300

    Implementation for BsTextArea

--- BootstrapForms/HtmlHelpers/InputExtensions.cs
+++ BootstrapForms/HtmlHelpers/InputExtensions.cs
@@ -102,13 +102,75 @@ namespace BootstrapForms.HtmlHelpers
 
             return MvcHtmlString.Create(helper.TextBoxFor(expression, htmlAttributes).ToHtmlString() + description.ToHtmlString());
         }
+
         public static MvcHtmlString BsTextBoxFor<TModel, TProperty>(this HtmlHelper<TModel> helper, Expression<Func<TModel, TProperty>> expression)
         {
             return BsTextBoxFor(helper, expression, (object)null);
         }
+
         public static MvcHtmlString BsTextBoxFor<TModel, TProperty>(this HtmlHelper<TModel> helper, Expression<Func<TModel, TProperty>> expression, object htmlAttributes)
         {
             return BsTextBoxFor(helper, expression, new RouteValueDictionary(htmlAttributes));
+        }
+
+        public static MvcHtmlString BsTextArea(this HtmlHelper htmlHelper, string name)
+        {
+            return BsTextArea(htmlHelper, name, null /* value */, null /* htmlAttributes */);
+        }
+
+        public static MvcHtmlString BsTextArea(this HtmlHelper htmlHelper, string name, object htmlAttributes)
+        {
+            return BsTextArea(htmlHelper, name, null /* value */, HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+        }
+
+        public static MvcHtmlString BsTextArea(this HtmlHelper htmlHelper, string name, IDictionary<string, object> htmlAttributes)
+        {
+            return BsTextArea(htmlHelper, name, null /* value */, htmlAttributes);
+        }
+
+        public static MvcHtmlString BsTextArea(this HtmlHelper htmlHelper, string name, string value)
+        {
+            return BsTextArea(htmlHelper, name, value, null /* htmlAttributes */);
+        }
+
+        public static MvcHtmlString BsTextArea(this HtmlHelper htmlHelper, string name, string value, object htmlAttributes)
+        {
+            return BsTextArea(htmlHelper, name, value, HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+        }
+
+        public static MvcHtmlString BsTextArea(this HtmlHelper htmlHelper, string name, string value, IDictionary<string, object> htmlAttributes)
+        {
+            return BsTextArea(htmlHelper, name, value, 2, 20, HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+        }
+
+        public static MvcHtmlString BsTextArea(this HtmlHelper htmlHelper, string name, string value, int rows, int columns, object htmlAttributes)
+        {
+            return BsTextArea(htmlHelper, name, value, rows, columns, HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+        }
+
+        public static MvcHtmlString BsTextArea(this HtmlHelper htmlHelper, string name, string value, int rows, int columns, IDictionary<string, object> htmlAttributes)
+        {
+            ModelMetadata metadata = ModelMetadata.FromStringExpression(name, htmlHelper.ViewContext.ViewData);
+            if (value != null)
+            {
+                metadata.Model = value;
+            }
+
+            //add placeholder           
+            if (!string.IsNullOrEmpty(metadata.Watermark) && !htmlAttributes.ContainsKey("placeholder"))
+            {
+                htmlAttributes.MergeAttribute("placeholder", metadata.Watermark);
+            }
+
+            //add html5 input type
+            if (!string.IsNullOrEmpty(metadata.DataTypeName))
+            {
+                htmlAttributes.MergeAttribute("type", metadata.DataTypeName.GetHtml5Type());
+            }
+
+            //merge custom css classes with bootstrap
+            htmlAttributes.MergeAttribute("class", "form-control");
+            return htmlHelper.TextArea(name, value, rows, columns, htmlAttributes);
         }
 
         /// <summary>
