be71cbfe2d3484faa30b3410109280577e19c767
Cezar Cretu <cretucezarandrei@gmail.com>
2013-08-14 02:53:29 +0300

    added test method used to debug symbolsource, will delete after it's done

---
+++ BootstrapForms/Models/TestMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootstrapForms.Models
+{
+    //public class with behavior, will be deleted
+    public class TestMethod
+    {
+        public string GetString()
+        {
+            return "test";
+        }
+    }
+}
