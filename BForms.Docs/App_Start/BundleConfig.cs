04dd4be2c85a86a0d6863fc6089d7b90f50de7a4
Stefan Prodan <stefan.prodan@gmail.com>
2013-09-07 11:47:07 +0300

    Fix CSS bundles

--- BForms.Docs/App_Start/BundleConfig.cs
+++ BForms.Docs/App_Start/BundleConfig.cs
@@ -11,7 +11,6 @@ namespace BForms.Docs
             bundles.Add(new StyleBundle("~/css")
                 .Include("~/Scripts/BForms/Components/Bootstrap/css/*.css", new CssRewriteUrlTransform())
                 .Include("~/Scripts/BForms/Plugins/Select2/css/*.css", new CssRewriteUrlTransform())
-                .Include("~/Scripts/BForms/Plugins/Datepicker/css/*.css", new CssRewriteUrlTransform())
                 .Include("~/Scripts/BForms/Stylesheets/*.css", new CssRewriteUrlTransform())
                 .Include("~/Content/Stylesheets/*.css", new CssRewriteUrlTransform())
                 );
