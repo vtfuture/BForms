9f267f518070ec05c4450028cd9b5a2038e840e3
cristipufu <cristi.pufu@yahoo.com>
2013-10-09 15:19:09 +0300

    added icons.css + images + glyphicons to grid

--- BForms.Docs/Areas/Demo/Helpers/Utils.cs
+++ BForms.Docs/Areas/Demo/Helpers/Utils.cs
@@ -5,11 +5,23 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BootstrapForms.Models;
+using BForms.Docs.Areas.Demo.Models;
 
 namespace BForms.Docs.Areas.Demo.Helpers
 {
-    public class Utils
+    public static class Utils
     {
-
+        public static string GetRoleIcon<T>(this HtmlHelper<T> helper, ProjectRole role)
+        {
+            switch (role)
+            {
+                case ProjectRole.TeamLeader:
+                    return "icn-three_stars";
+                case ProjectRole.Developer:
+                    return "icn-two_stars";
+                default:
+                    return "icn-one_star";
+            }
+        }
     }
 }
