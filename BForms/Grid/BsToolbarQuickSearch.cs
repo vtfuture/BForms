73512e2403e2baa667e13f2a2f81c918bac51977
CiprianPorumbescu <ciprian.porumbescu@live.com>
2013-10-11 14:43:43 +0300

    added glyphIcon option to BsToolbarQuickSearch

--- BForms/Grid/BsToolbarQuickSearch.cs
+++ BForms/Grid/BsToolbarQuickSearch.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BForms.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,22 +10,35 @@ namespace BForms.Grid
 {
     public class BsToolbarQuickSearch : BaseComponent
     {
+        private Glyphicon? glyphIcon;
+
         public BsToolbarQuickSearch() { }
 
         public BsToolbarQuickSearch(ViewContext viewContext)
             : base(viewContext) { }
+
+        public BsToolbarQuickSearch GlyphIcon(Glyphicon icon)
+        {
+            this.glyphIcon = icon;
+            return this;
+        }
 
         public override string Render()
         {
             var inputGroupBuilder = new TagBuilder("div");
             inputGroupBuilder.MergeAttribute("class", "input-group");
 
+            if (this.glyphIcon.HasValue)
+            {
+                inputGroupBuilder.InnerHtml += GetGlyphcon(this.glyphIcon.Value);
+            }
+
             var inputBuilder = new TagBuilder("input");
             inputBuilder.MergeAttribute("class", "form-control bs-text");
             inputBuilder.MergeAttribute("type", "text");
             inputBuilder.MergeAttribute("id", "quick_search");
 
-            inputGroupBuilder.InnerHtml = inputBuilder.ToString(TagRenderMode.SelfClosing);
+            inputGroupBuilder.InnerHtml += inputBuilder.ToString(TagRenderMode.SelfClosing);
 
             return inputGroupBuilder.ToString();
         }
