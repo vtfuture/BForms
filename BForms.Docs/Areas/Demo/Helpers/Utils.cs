4ff8af196d03d1fb5d2a8718c7ebdb94164fa183
Stefan Prodan <stefan.prodan@gmail.com>
2013-10-18 13:35:58 +0300

    Added build version to docs footer

--- BForms.Docs/Areas/Demo/Helpers/Utils.cs
+++ BForms.Docs/Areas/Demo/Helpers/Utils.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 using BForms.Models;
 using BForms.Html;
 using BForms.Docs.Areas.Demo.Models;
+using System.Reflection;
 
 namespace BForms.Docs.Areas.Demo.Helpers
 {
@@ -34,6 +35,47 @@ namespace BForms.Docs.Areas.Demo.Helpers
             var monthName = dateTimeFormat.GetMonthName(month);
 
             return string.Format("{0} {1}", monthName, current.Year);
+        }
+
+        public static string BFromsVersion()
+        {
+            try
+            {
+                var version = Assembly.GetExecutingAssembly().GetReferencedAssemblies().Where(a => a.Name.Contains("BForms")).First().Version.ToString();
+
+                string filePath = System.Reflection.Assembly.GetCallingAssembly().Location;
+                const int c_PeHeaderOffset = 60;
+                const int c_LinkerTimestampOffset = 8;
+                byte[] b = new byte[2048];
+                System.IO.Stream s = null;
+
+                try
+                {
+                    s = new System.IO.FileStream(filePath, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+                    s.Read(b, 0, 2048);
+                }
+                finally
+                {
+                    if (s != null)
+                    {
+                        s.Close();
+                    }
+                }
+
+                int i = System.BitConverter.ToInt32(b, c_PeHeaderOffset);
+                int secondsSince1970 = System.BitConverter.ToInt32(b, i + c_LinkerTimestampOffset);
+                DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0);
+                dt = dt.AddSeconds(secondsSince1970);
+                dt = dt.AddHours(TimeZone.CurrentTimeZone.GetUtcOffset(dt).Hours);
+
+                return "v" + version + " at " + dt.ToString("dd.MM.yyyy hh:mm");
+            }
+            catch (Exception)
+            {
+
+            }
+
+            return "v1.0.0";
         }
     }
 }
