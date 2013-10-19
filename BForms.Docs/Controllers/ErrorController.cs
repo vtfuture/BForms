cc745f9792dfe0b667f164598a9fb36c80dc8011
Stefan Prodan <stefan.prodan@gmail.com>
2013-10-20 00:14:19 +0300

    Custom error page added to docs site

---
+++ BForms.Docs/Controllers/ErrorController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BForms.Docs.Controllers
+{
+    public class ErrorController : BaseController
+    {
+        //
+        // GET: /Error/
+        public ActionResult Index()
+        {
+            return View("Error");
+        }
+
+        public ViewResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View("NotFound");
+        }
+	}
+}
