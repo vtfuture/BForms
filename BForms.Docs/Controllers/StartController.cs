6e8bae9a047922b93e7c8d5b41a982332c28634a
Stefan Prodan <stefan.prodan@gmail.com>
2013-10-19 14:18:42 +0300

    Fix for Start controller

---
+++ BForms.Docs/Controllers/StartController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BForms.Docs.Controllers
+{
+    public class StartController : BaseController
+    {
+
+        public ActionResult Setup()
+        {
+            return View("Setup/Index");
+        }
+
+        public ActionResult Form()
+        {
+            return View("Form/Index");
+        }
+
+        public ActionResult Grid()
+        {
+            return View("Grid/Index");
+        }
+    }
+}
