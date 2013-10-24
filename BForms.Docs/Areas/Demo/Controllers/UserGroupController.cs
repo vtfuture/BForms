1aa5946c0506b0c7af1f4dcfc7b1399a260363a7
Stefan Prodan <stefan.prodan@gmail.com>
2013-10-24 17:52:21 +0300

    Added GroupEditor empty demo page

---
+++ BForms.Docs/Areas/Demo/Controllers/UserGroupController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.ComponentModel.DataAnnotations;
+using BForms.Models;
+using BForms.Mvc;
+using BForms.Utilities;
+using BForms.Docs.Areas.Demo.Mock;
+using BForms.Docs.Areas.Demo.Models;
+using BForms.Docs.Areas.Demo.Helpers;
+using BForms.Docs.Controllers;
+
+namespace BForms.Docs.Areas.Demo.Controllers
+{
+    public class UserGroupController : BaseController
+    {
+        //
+        // GET: /Demo/UserGroup/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
