ab3627a9cdf1d4c3df2b2210d81f3ebfe863e400
Stefan Prodan <stefan.prodan@gmail.com>
2013-11-16 15:51:12 +0200

    Added article meta to docs

--- BForms.Docs/Areas/Demo/Helpers/Utils.cs
+++ BForms.Docs/Areas/Demo/Helpers/Utils.cs
@@ -77,5 +77,46 @@ namespace BForms.Docs.Areas.Demo.Helpers
 
             return "v1.0.0";
         }
+
+        public static DateTime BFromsDate()
+        {
+            try
+            {
+                var version = Assembly.GetExecutingAssembly().GetReferencedAssemblies().Where(a => a.Name.Contains("BForms")).First().Version.ToString();
+
+                string filePath = System.Reflection.Assembly.GetCallingAssembly().Location;
+                const int c_PeHeaderOffset = 60;
+                const int c_LinkerTimestampOffset = 8;
+                byte[] b = new byte[2048];
+                System.IO.Stream s = null;
+
+                try
+                {
+                    s = new System.IO.FileStream(filePath, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+                    s.Read(b, 0, 2048);
+                }
+                finally
+                {
+                    if (s != null)
+                    {
+                        s.Close();
+                    }
+                }
+
+                int i = System.BitConverter.ToInt32(b, c_PeHeaderOffset);
+                int secondsSince1970 = System.BitConverter.ToInt32(b, i + c_LinkerTimestampOffset);
+                DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0);
+                dt = dt.AddSeconds(secondsSince1970);
+                dt = dt.AddHours(TimeZone.CurrentTimeZone.GetUtcOffset(dt).Hours);
+
+                return dt;
+            }
+            catch (Exception)
+            {
+
+            }
+
+            return DateTime.Now;
+        }
     }
 }
