668a7093d1b913a5b0766ab4c532ad92700af2a7
Cezar Cretu <cretucezarandrei@gmail.com>
2013-11-25 14:00:48 +0200

    added contrived example for BsHelpers problem with nested model

---
+++ BForms.EndToEnd/Models/Nested.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using BForms.Models;
+using BForms.Mvc;
+
+namespace BForms.EndToEnd.Models
+{
+    public class Car
+    {
+        public List<Tire> Tires { get; set; } 
+    }
+
+    public class Tire
+    {
+        public string Manufacturer { get; set; }
+        [BsControl(BsControlType.RadioButtonList)]
+        public BsSelectList<ConstructionType> ConstructionType { get; set; }
+    }
+
+    public enum ConstructionType
+    {
+        Bias,
+        BeltedBias,
+        Radial,
+        Solid,
+        Semipneumatic,
+        Airless
+    }
+}
