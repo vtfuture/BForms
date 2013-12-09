52c574a827b3f39edb2e06c340428443c5f6cc9b
cristipufu <cristi.pufu@yahoo.com>
2013-12-09 19:36:30 +0200

    bs page extensions

---
+++ BForms/Html/BsPagerExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using BForms.Grid;
+using BForms.Models;
+
+namespace BForms.Html
+{
+    public static class BsPagerExtensions
+    {
+        public static BsGridPagerBuilder BsPager(this HtmlHelper html, BsPagerModel model)
+        {
+            var builder = new BsGridPagerBuilder(model, null, null) { viewContext = html.ViewContext };
+
+            return builder;
+        }
+
+        public static BsGridPagerBuilder BsPagerFor<TModel>(this HtmlHelper<TModel> html, Expression<Func<TModel, BsPagerModel>> expression)
+        {
+            var model = expression.Compile().Invoke(html.ViewData.Model);
+            return BsPager(html, model);
+        }
+    }
+}
