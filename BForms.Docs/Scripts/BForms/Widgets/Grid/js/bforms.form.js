3f93b877a7e83759811147d81cb94ee299786b33
mariuscosareanu <cosareanu2@gmail.com>
2013-12-20 15:20:20 +0200

    added showErrors to bforms.form

--- BForms.Docs/Scripts/BForms/Widgets/Grid/js/bforms.form.js
+++ BForms.Docs/Scripts/BForms/Widgets/Grid/js/bforms.form.js
@@ -159,10 +159,11 @@
                     if (response != null && response.Errors != null) {
                         
                         if (typeof validatedForm === "undefined") {
-                            
                             $.validator.unobtrusive.parse(this.$form);
                             validatedForm = this.$form.validate();
                         }
+
+                        validatedForm.showErrors(response.Errors);
                     }
                 }
             });
