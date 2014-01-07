2309106341b500b7b1c553a326ce9f73be8a508b
mariuscosareanu <cosareanu2@gmail.com>
2014-01-07 19:33:53 +0200

    sortable to sortable dragging support

--- BForms.Docs/Scripts/BForms/Widgets/GroupEditor/bforms.groupEditor.js
+++ BForms.Docs/Scripts/BForms/Widgets/GroupEditor/bforms.groupEditor.js
@@ -7,10 +7,10 @@
     'bforms-namespace',
     'bforms-inlineQuestion',
     'bforms-form'
-], function(ichSingleton) {
+], function (ichSingleton) {
 
     //#region Constructor and Properties
-    var GroupEditor = function(opt) {
+    var GroupEditor = function (opt) {
         this.options = $.extend(true, {}, this.options, opt);
         this._init();
     };
@@ -48,7 +48,7 @@
     //#endregion
 
     //#region Init
-    GroupEditor.prototype._init = function() {
+    GroupEditor.prototype._init = function () {
 
         this.$element = this.element;
 
@@ -73,20 +73,19 @@
         this._initSortable();
     };
 
-    GroupEditor.prototype._initComponents = function() {
-        this.renderer = ichSingleton.getInstance();
+    GroupEditor.prototype._initComponents = function () {
         this._countGroupItems();
         this._checkEditable();
     };
 
-    GroupEditor.prototype._initSelectors = function() {
+    GroupEditor.prototype._initSelectors = function () {
         this.$navbar = this.$element.find(this.options.navbarSelector);
         this.$tabs = this.$element.find(this.options.tabsSelector);
         this.$groups = this.$element.find(this.options.groupsSelector);
         this.$counter = this.$groups.find(this.options.groupItemsCounter);
     };
 
-    GroupEditor.prototype._addDelegates = function() {
+    GroupEditor.prototype._addDelegates = function () {
         this.$navbar.on('click', 'a', $.proxy(this._evChangeTab, this));
         this.$tabs.find('div[data-tabid]').on('click', 'button' + this.options.toolbarBtnSelector, $.proxy(this._evChangeToolbarForm, this));
         this.$tabs.on('click', this.options.addBtn, $.proxy(this._evAdd, this));
@@ -97,21 +96,21 @@
         this.$groups.on('click', this.options.toggleExpandBtn, $.proxy(this._evToggleExpand, this));
     };
 
-    GroupEditor.prototype._initSelectedTab = function() {
+    GroupEditor.prototype._initSelectedTab = function () {
         this._initTab(this._getSelectedTab());
     };
 
-    GroupEditor.prototype._initTabForms = function() {
+    GroupEditor.prototype._initTabForms = function () {
         var forms = this.$element.find(this.options.editorFormSelector);
 
-        $.each(forms, function(idx) {
+        $.each(forms, function (idx) {
             $(this).bsForm({
                 uniqueName: $(this).data('uid') + idx
             });
         });
     };
 
-    GroupEditor.prototype._initTab = function(tabModel) {
+    GroupEditor.prototype._initTab = function (tabModel) {
         var self = this;
 
         if (tabModel) {
@@ -133,7 +132,7 @@
             if (!tabModel.pager) {
                 tabModel.pager = tabModel.container.find(this.options.pagerSelector);
                 tabModel.pager.bsPager({
-                    pagerUpdate: function(e, data) {
+                    pagerUpdate: function (e, data) {
                         self._evChangePage(data, tabModel);
                     },
                     pagerGoTop: $.proxy(this._evGoTop, this)
@@ -150,7 +149,7 @@
         }
     };
 
-    GroupEditor.prototype._initSortable = function() {
+    GroupEditor.prototype._initSortable = function () {
         this.$element.find(this.options.groupSelector).sortable({
             items: this.options.groupItemSelector,
             distance: 5,
@@ -161,15 +160,13 @@
         });
     };
 
-    GroupEditor.prototype._initDraggable = function(tabModel) {
-        console.log(tabModel);
-
+    GroupEditor.prototype._initDraggable = function (tabModel) {
         tabModel.container.find(this.options.tabItemSelector).draggable(this._getDraggableOptions(tabModel));
     };
     //#endregion
 
     //#region Events
-    GroupEditor.prototype._evUp = function(e) {
+    GroupEditor.prototype._evUp = function (e) {
         e.preventDefault();
         var $item = $(e.currentTarget).parents(this.options.groupItemSelector).first(),
             $prevItem = $item.prevAll(this.options.groupItemSelector).first();
@@ -182,7 +179,7 @@
         }
     };
 
-    GroupEditor.prototype._evDown = function(e) {
+    GroupEditor.prototype._evDown = function (e) {
         e.preventDefault();
         var $item = $(e.currentTarget).parents(this.options.groupItemSelector).first(),
             $nextElem = $item.nextAll(this.options.groupItemSelector).first();
@@ -195,17 +192,17 @@
         }
     };
 
-    GroupEditor.prototype._evToggleExpand = function(e) {
+    GroupEditor.prototype._evToggleExpand = function (e) {
         e.preventDefault();
         var $el = $(e.currentTarget),
             $group = $el.parents('[data-groupid]'),
             $container = $group.find(this.options.groupItemsWrapper);
-        $container.toggle('fast', $.proxy(function() {
+        $container.toggle('fast', $.proxy(function () {
             $group.find(this.options.toggleExpandBtn).toggleClass('open');
         }, this));
     };
 
-    GroupEditor.prototype._evChangeToolbarForm = function(e) {
+    GroupEditor.prototype._evChangeToolbarForm = function (e) {
         e.preventDefault();
         var $el = $(e.currentTarget),
             uid = $el.data('uid'),
@@ -221,7 +218,7 @@
         }
     };
 
-    GroupEditor.prototype._evChangeTab = function(e) {
+    GroupEditor.prototype._evChangeTab = function (e) {
         e.preventDefault();
         var $el = $(e.currentTarget),
             tabId = $el.data('tabid'),
@@ -237,7 +234,7 @@
         });
     };
 
-    GroupEditor.prototype._evChangePage = function(data, tabModel) {
+    GroupEditor.prototype._evChangePage = function (data, tabModel) {
         this._ajaxGetTab({
             tabModel: tabModel,
             data: {
@@ -248,13 +245,13 @@
         });
     };
 
-    GroupEditor.prototype._evGoTop = function(e) {
+    GroupEditor.prototype._evGoTop = function (e) {
         e.preventDefault();
 
         console.log(" -- go to top --", arguments);
     };
 
-    GroupEditor.prototype._evRemove = function(e) {
+    GroupEditor.prototype._evRemove = function (e) {
         e.preventDefault();
         var $el = $(e.currentTarget),
             $item = $el.parents(this.options.groupItemSelector),
@@ -266,7 +263,7 @@
         this._countGroupItems();
     };
 
-    GroupEditor.prototype._evAdd = function(e) {
+    GroupEditor.prototype._evAdd = function (e) {
         e.preventDefault();
         var $el = $(e.currentTarget),
             objId = $el.parents(this.options.tabItemSelector).data('objid'),
@@ -275,14 +272,14 @@
             connectsWith = tabModel.connectsWith,
             $groups = this._getGroups(connectsWith);
 
-        $.each($groups, $.proxy(function(idx, group) {
+        $.each($groups, $.proxy(function (idx, group) {
             if (!this._isInGroup(objId, tabId, $(group))) {
 
-                var $template = this._getGroupItemTemplate($(group), tabId, objId),
-                    tabItem = this._getTabItem(tabId, objId),
-                    model = tabItem.data('model');
+                var tabItem = this._getTabItem(tabId, objId),
+                    model = tabItem.data('model'),
+                    $template = this._getGroupItemTemplate($(group), tabId, objId, model);
 
-                var view = this.renderer['js-groupItem'](model);
+                var view = this._renderGroupItem(model, group, tabId, objId);
 
                 $template.find(this.options.groupItemContentSelector).html(view);
 
@@ -299,7 +296,7 @@
         this._countGroupItems();
     };
 
-    GroupEditor.prototype._evEdit = function(e) {
+    GroupEditor.prototype._evEdit = function (e) {
         e.preventDefault();
         var $el = $(e.currentTarget),
             $item = $el.parents(this.options.groupItemSelector),
@@ -312,7 +309,7 @@
     //#endregion
 
     //#region Ajax
-    GroupEditor.prototype._ajaxGetTab = function(param) {
+    GroupEditor.prototype._ajaxGetTab = function (param) {
         var ajaxOptions = {
             name: this.options.uniqueName + "|getTab",
             url: this.options.getTabUrl,
@@ -328,7 +325,7 @@
         $.bforms.ajax(ajaxOptions);
     };
 
-    GroupEditor.prototype._ajaxGetTabSuccess = function(response, callback) {
+    GroupEditor.prototype._ajaxGetTabSuccess = function (response, callback) {
         if (response) {
 
             var container = callback.tabModel.container;
@@ -345,55 +342,55 @@
         }
     };
 
-    GroupEditor.prototype._ajaxGetTabError = function() {
+    GroupEditor.prototype._ajaxGetTabError = function () {
         console.trace();
     };
     //#endregion
 
     //#region Helpers
-    GroupEditor.prototype._checkEditable = function() {
-        $.each(this._getGroupItems(), $.proxy(function(idx, item) {
+    GroupEditor.prototype._checkEditable = function () {
+        $.each(this._getGroupItems(), $.proxy(function (idx, item) {
             this._checkEditableItem($(item));
         }, this));
     };
 
-    GroupEditor.prototype._checkEditableItem = function($item) {
+    GroupEditor.prototype._checkEditableItem = function ($item) {
         if (this._isTabEditable($item.data('tabid'))) {
             $item.find(this.options.editBtn).show();
         }
     };
 
-    GroupEditor.prototype._rebuildNumbers = function() {
+    GroupEditor.prototype._rebuildNumbers = function () {
 
     };
 
-    GroupEditor.prototype._shakeElement = function($element) {
+    GroupEditor.prototype._shakeElement = function ($element) {
         $element.effect('shake', { times: 2, direction: 'up', distance: 10 }, 50);
     };
 
-    GroupEditor.prototype._countGroupItems = function() {
+    GroupEditor.prototype._countGroupItems = function () {
         this.$counter.html(this._getGroupItems().length);
     };
 
-    GroupEditor.prototype._checkItem = function($item, tabId, connectsWith) {
+    GroupEditor.prototype._checkItem = function ($item, tabId, connectsWith) {
         if (this._isItemSelected($item.data('objid'), tabId, connectsWith)) {
             this._toggleItemCheck($item, false);
         }
     };
 
-    GroupEditor.prototype._checkItems = function() {
+    GroupEditor.prototype._checkItems = function () {
         var selectedTab = this._getSelectedTab(),
             $items = selectedTab.container.find(this.options.tabItemSelector);
 
-        $.each($items, $.proxy(function(idx, item) {
+        $.each($items, $.proxy(function (idx, item) {
             this._checkItem($(item), selectedTab.tabId, selectedTab.connectsWith);
         }, this));
     };
 
-    GroupEditor.prototype._isItemSelected = function(objid, tabId, groupIds) {
+    GroupEditor.prototype._isItemSelected = function (objid, tabId, groupIds) {
         var $groups = this._getGroups(groupIds), selected = true;
 
-        $.each($groups, $.proxy(function(idx, group) {
+        $.each($groups, $.proxy(function (idx, group) {
             if (!this._isInGroup(objid, tabId, $(group))) {
                 selected = false;
             }
@@ -402,11 +399,11 @@
         return selected;
     };
 
-    GroupEditor.prototype._isInGroup = function(objId, tabId, $group) {
+    GroupEditor.prototype._isInGroup = function (objId, tabId, $group) {
         var isInGroup = false,
             $groupItems = $group.find(this.options.groupItemSelector);
 
-        $.each($groupItems, function(idx, item) {
+        $.each($groupItems, function (idx, item) {
             if ($(item).data('objid') == objId && $(item).data('tabid') == tabId) {
                 isInGroup = true;
                 return false;
@@ -415,7 +412,7 @@
         return isInGroup;
     };
 
-    GroupEditor.prototype._toggleItemCheck = function($item, forceUncheck) {
+    GroupEditor.prototype._toggleItemCheck = function ($item, forceUncheck) {
         var $glyph = $item.find('span.glyphicon'),
             addBtn = this.options.addBtn.replace(".", "");
 
@@ -439,7 +436,7 @@
         }
     };
 
-    GroupEditor.prototype._getSelectedTab = function() {
+    GroupEditor.prototype._getSelectedTab = function () {
         var $container = this.$tabs.find('div[data-tabid]:visible'),
             tabId = $container.data('tabid'),
             connectsWith = $container.data('connectswith'),
@@ -453,47 +450,47 @@
         };
     };
 
-    GroupEditor.prototype._hideTabs = function() {
+    GroupEditor.prototype._hideTabs = function () {
 
         var $containers = this.$tabs.find('div[data-tabid]');
 
         $containers.hide();
     };
 
-    GroupEditor.prototype._isLoaded = function($element) {
+    GroupEditor.prototype._isLoaded = function ($element) {
 
         var dataLoaded = $element.data('loaded');
 
         return dataLoaded == "true" || dataLoaded == "True" || dataLoaded == true;
     };
 
-    GroupEditor.prototype._getTab = function(tabId) {
+    GroupEditor.prototype._getTab = function (tabId) {
         return this.$tabs.find('div[data-tabid="' + tabId + '"]');
     };
 
-    GroupEditor.prototype._isTabEditable = function(tabId) {
+    GroupEditor.prototype._isTabEditable = function (tabId) {
         return this._getTab(tabId).data('editable');
     };
 
-    GroupEditor.prototype._getTabItem = function(tabId, objId) {
+    GroupEditor.prototype._getTabItem = function (tabId, objId) {
         var $container = this._getTab(tabId),
             $item = $container.find(this.options.tabItemSelector + '[data-objid="' + objId + '"]');
         return $item;
     };
 
-    GroupEditor.prototype._getGroup = function(groupId) {
+    GroupEditor.prototype._getGroup = function (groupId) {
         return this.$groups.find('div[data-groupid="' + groupId + '"]');
     };
 
-    GroupEditor.prototype._getGroupItems = function() {
+    GroupEditor.prototype._getGroupItems = function () {
         return this.$groups.find(this.options.groupItemSelector + ':not(' + this.options.groupItemTemplateSelector + ' >)');
     };
 
-    GroupEditor.prototype._getGroups = function(groupIds) {
+    GroupEditor.prototype._getGroups = function (groupIds) {
         var $groups;
         if (groupIds) {
             $groups = [];
-            $.each(groupIds, $.proxy(function(idx, groupId) {
+            $.each(groupIds, $.proxy(function (idx, groupId) {
                 $groups.unshift(this._getGroup(groupId));
             }, this));
         } else {
@@ -502,22 +499,41 @@
         return $groups;
     };
 
-    GroupEditor.prototype._getGroupItemTemplate = function($group, tabId, objId) {
+    GroupEditor.prototype._getGroupItemTemplate = function ($group, tabId, objId, model) {
         var template = $group.find(this.options.groupItemTemplateSelector).children(':first').clone();
+
         template.data('objid', objId);
         template.data('tabid', tabId);
+        template.data('model', model);
+
         var html = template.html();
+
         html = html.replace(/__tabid__/gi, tabId);
         html = html.replace(/{{tabid}}/gi, tabId);
         html = html.replace(/__objid__/gi, tabId);
         html = html.replace(/{{objid}}/gi, objId);
+
         template.html(html);
+
         return template;
+    };
+
+    GroupEditor.prototype._renderGroupItem = function (model, group, tabId, objId) {
+
+        if (typeof this.options.buildGroupItem === "function") {
+            return this.options.buildGroupItem(model, group, tabId, objId);
+        } else {
+            if (typeof model.Name === "undefined") {
+                throw "No buildGroupItem method define and no Name property found on object";
+            } else {
+                return model.Name;
+            }
+        }
     };
     //#endregion
 
     //#region Dragable & Sortable helpers
-    GroupEditor.prototype._getDraggableOptions = function(tabModel) {
+    GroupEditor.prototype._getDraggableOptions = function (tabModel) {
 
         return {
             distance: 5,
@@ -528,25 +544,25 @@
                 left: typeof this.options.getCursorAtLeft === "function" ? this.options.getCursorAtLeft(tabModel) : 0,
             },
             start: $.proxy(this._dragStart, this),
-            stop: $.proxy(this._dragStop, this)
+            stop: $.proxy(this._dragStop, this),
+            cancel : '.selected'
         };
     };
 
-    GroupEditor.prototype._buildDragElement = function(e) {
+    GroupEditor.prototype._buildDragElement = function (e) {
+
         var $dragged = $(e.currentTarget),
             model = $dragged.data('model'),
             $tab = $dragged.parents('[data-tabid]'),
             tabId = $tab.data('tabid'),
             connectsWith = $tab.data('connectswith');
 
-        console.log($dragged);
-
         var $draggedHelper = $(this.options.buildDragHelper(model, tabId, connectsWith));
 
         return $draggedHelper;
     };
 
-    GroupEditor.prototype._buildConnectsWithSelector = function(allowed) {
+    GroupEditor.prototype._buildConnectsWithSelector = function (allowed) {
         var selector = '';
         selector += this.options.groupSelector;
         for (var key in allowed) {
@@ -561,20 +577,74 @@
 
     GroupEditor.prototype._dragStart = function () {
 
+        this._dragging = true;
+
     };
 
     GroupEditor.prototype._dragStop = function () {
-    };
 
-    GroupEditor.prototype._sortStart = function() {
-
-    };
-
-    GroupEditor.prototype._beforeSortStop = function() {
+        this._dragging = false;
 
     };
 
-    GroupEditor.prototype._sortStop = function() {
+    GroupEditor.prototype._sortStart = function (e, ui) {
+
+        var $item = ui.item,
+            groupId = $item.parents(this.options.groupSelector).data('groupid');
+
+        $item.data('groupid', groupId);
+    };
+
+    GroupEditor.prototype._beforeSortStop = function () {
+
+    };
+
+    GroupEditor.prototype._sortStop = function (e, ui) {
+
+        var $item = ui.item,
+            $group = $item.parents(this.options.groupSelector).first(),
+            tabModel = this._getSelectedTab(),
+            connectsWith = tabModel.connectsWith,
+            model = $item.data('model'),
+            tabId = tabModel.tabId,
+            objId = $item.data('objid'),
+            allowMove = false;
+
+        if (this._dragging === true) {
+
+            //dragged from tabs to groups
+            if (!this._isInGroup(objId, tabId, $group)) {
+                allowMove = true;
+            } else {
+                ui.item.remove();
+            }
+
+        } else {
+
+            //inner group sorting or moving from one group to another
+            var sourceGroupId = $item.data('groupid'),
+                targetGroupId = $group.data('groupid');
+
+            if (sourceGroupId == targetGroupId) return;
+            else {
+                allowMove = true;
+            }
+        }
+
+        if (allowMove) {
+            var $template = this._getGroupItemTemplate($group, tabId, objId, model);
+
+            var view = this._renderGroupItem(model, $group, tabId, objId);
+
+            $template.find(this.options.groupItemContentSelector).html(view);
+
+            this._checkEditableItem($template);
+
+            ui.item.replaceWith($template);
+
+            this._checkItem(this._getTabItem(tabId, objId), tabId, connectsWith);
+        }
+
     };
     //#endregion
 
